03cba714991cc041c8cb0f3c55c25c1415a3b0ea
Alex Sarabia <alexsarabia201@gmail.com>
2022-06-22 13:38:19 -0500

    Clase de JWT

--- src/main/java/com/idat/MayoServicioPrueba/security/ConfigSecurity.java
+++ src/main/java/com/idat/MayoServicioPrueba/security/ConfigSecurity.java
@@ -1,36 +1,60 @@
 package com.idat.MayoServicioPrueba.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 
-
+	@Autowired
+	private UserDetailService service;
+	
+	@Autowired
+	private TokenFilter filter;
+	
+	@Autowired
+	private EntryPoint entrypoint;
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("alexis").password(encriptado().encode("123")).roles("admin");
-		auth.inMemoryAuthentication().withUser("jorman").password(encriptado().encode("123")).roles("estudiante");
-		
+	//	auth.inMemoryAuthentication().withUser("alexis").password(encriptado().encode("123")).roles("admin");
+		//auth.inMemoryAuthentication().withUser("jorman").password(encriptado().encode("123")).roles("estudiante");
+		auth.userDetailsService(service).passwordEncoder(encriptado());
 
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		
-		http.authorizeRequests().antMatchers("/producto/v1/**").access("hasRole('admin')")
+		//http.authorizeRequests().antMatchers("/producto/v1/**").access("hasRole('admin')")
+		//.and()
+		//.httpBasic()
+		//.and()
+		//.csrf().disable();
+		http.authorizeRequests().
+		antMatchers("/crearToken").permitAll().
+		anyRequest().
+		authenticated().
+		and().
+		exceptionHandling().
+		authenticationEntryPoint(entrypoint).
+		and().
+		sessionManagement().
+		sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 		.and()
-		.httpBasic()
-		.and()
-		.csrf().disable();
+		.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+		.csrf().disable() ;
 		
 	}
 	
@@ -39,6 +63,20 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter {
 		return new BCryptPasswordEncoder();
 		
 	}
+
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
+	}
+
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManager();
+	}
+	
+	
 
 
 	
